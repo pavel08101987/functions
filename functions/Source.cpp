997751fa209613f30e7a01569656750a21fa35f9
Pere_pg86@itstepru.onmicrosoft.com <Pere_pg86@itstepru.onmicrosoft.com>
2021-03-22 23:26:27 +0300

    functions overload

--- functions/Source.cpp
+++ functions/Source.cpp
@@ -7,6 +7,13 @@ void fillRand(int arr[], const int n)
 		arr[i] = rand();
 	}
 }
+void fillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand();
+	}
+}
 
 void Print(int arr[], const int n)
 {
@@ -17,8 +24,31 @@ void Print(int arr[], const int n)
 	cout << endl;
 
 }
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
 
 void ShiftLeft(int arr[], const int n, const int number)
+{
+	for (int i = 0; i < number; i++)
+	{
+		int buffer = arr[0];
+		for (int j = 0; j < n; j++)
+		{
+			arr[j] = arr[j + 1];
+
+		}
+		arr[n - 1] = buffer;
+
+	}
+
+}
+void ShiftLeft(double arr[], const int n, const int number)
 {
 	for (int i = 0; i < number; i++)
 	{
@@ -48,19 +78,41 @@ void ShiftR(int arr[], const int n, const int number)
 
 	}
 }
+void ShiftR(double arr[], const int n, const int number)
+{
+	for (int i = 0; i < number; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int j = n - 1; j > 0; j--)
+		{
+			arr[j] = arr[j - 1];
 
-void Sum(int arr[], const int n)
+		}
+		arr[0] = buffer;
+
+	}
+}
+
+int Sum(int arr[], const int n)
 {
 	int sum = 0;
 	for (int i = 0; i < n; i++)
 	{
      sum += arr[i];
 	}
-	cout << "сумма элементов массивы: " << sum << endl;
-
+	return sum;
+}
+double Sum(double arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
 }
 
-void Avg(int arr[], const int n)
+int Avg(int arr[], const int n)
 {
 	double sumsr = 0;
 	for (int i = 0; i < n; i++)
@@ -68,10 +120,22 @@ void Avg(int arr[], const int n)
 		sumsr += arr[i];
 
 	}
-	cout << "среднеарифметическое массива: " << sumsr / n << endl;
+	return sumsr;
+
+}
+double Avg(double arr[], const int n)
+{
+	double sumsr = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sumsr += arr[i];
+
+	}
+	return sumsr;
+
 }
 
-void minValueIn(int arr[], const int n)
+int minValueIn(int arr[], const int n)
 {
 	int min_number = arr[0];
 	for (int i = 0; i < n; i++)
@@ -82,10 +146,23 @@ void minValueIn(int arr[], const int n)
 		}
 
 	}
-	cout << "минимальное число: " << min_number << endl;
+	return min_number;
+}
+double minValueIn(double arr[], const int n)
+{
+	double min_number = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min_number)
+		{
+			min_number = arr[i];
+		}
+
+	}
+	return min_number;
 }
 
-void maxValueIn(int arr[], const int n)
+int maxValueIn(int arr[], const int n)
 {
 	int max_number = arr[0];
 	for (int i = 0; i < n; i++)
@@ -96,8 +173,22 @@ void maxValueIn(int arr[], const int n)
 		}
 
 	}
-	cout << "максимальное число: " << max_number << endl;
+	
+	return max_number;
+}
+double maxValueIn(double arr[], const int n)
+{
+	double max_number = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max_number)
+		{
+			max_number = arr[i];
+		}
 
+	}
+	
+	return max_number;
 }
 
 void Sort(int arr[], const int n)
@@ -109,6 +200,23 @@ void Sort(int arr[], const int n)
 			if (arr[j] < arr[i])
 			{
 				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+
+	}
+
+}
+void Sort(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				double buffer = arr[i];
 				arr[i] = arr[j];
 				arr[j] = buffer;
 			}
@@ -177,6 +285,62 @@ void main()
 	cout << endl;
 	Sort(arr, n);
 	Print(arr, n);
+
+	double arr[n];
+	/*for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand();
+
+	}*/
+	fillRand(arr, n);
+	/*for (int i = 0; i < n; i++)
+	{
+		cout << arr[i]<<" ";
+
+	}
+	cout << endl;*/
+	Print(arr, n);
+	Sum(arr, n);
+	Avg(arr, n);
+	minValueIn(arr, n);
+	maxValueIn(arr, n);
+
+	//сдвиг массива
+
+	double number;
+	cout << "введите количество: "; cin >> number;
+	/*for (int i = 0; i < number; i++)
+	{
+		int buffer = arr[0];
+		for (int j = 0; j < n; j++)
+		{
+			arr[j] = arr[j + 1];
+
+		}
+		arr[n - 1] = buffer;
+
+	}*/
+	ShiftLeft(arr, n, number);
+
+	/*for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+
+	}
+	cout << endl;*/
+	Print(arr, n);
+	cout << endl;
+	cout << "сдвиг в право: " << endl;
+	Print(arr, n);
+	cout << endl;
+	cout << "введите количество: "; cin >> number;
+	ShiftR(arr, n, number);
+	Print(arr, n);
+	cout << endl;
+	Sort(arr, n);
+	Print(arr, n);
+
+
 
 	////////
 
